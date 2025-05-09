7399946b701f0b037e3e6a5671a3610709ac0dfa
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-09 20:39:37 +0100

    chore: remove unused Button import from App component

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
