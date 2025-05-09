67f3a845cc27d36c1e13119d1f5bbf15218b7ce3
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-09 19:19:28 +0100

    feat: implement product routes for CRUD operations

--- api/server.js
+++ api/server.js
@@ -1,16 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from the server!");
-});
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
