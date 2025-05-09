a5bf2a58703b9336294797ec2f5c6b52a3120655
Gokul Kannan <gokulskannan1993@gmail.com>
2025-05-09 15:20:35 +0100

    feat: add root route to respond with a greeting message

--- api/server.js
+++ api/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
+});
+
 app.listen(5000, () => {
   console.log("Server is running on port http://localhost:5000");
 });
